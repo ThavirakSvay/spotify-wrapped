d619d876be2f02202e5d51ee8bc3f102db9e0910
ThavirakSvay <thaavirak@gmail.com>
2021-12-14 09:49:08 +0700

    update

--- index.js
+++ index.js
@@ -73,9 +73,12 @@ app.get("/callback", (req, res) => {
 		});
 });
 
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+if (process.env.NODE_ENV) {
+	app.use(express.static(path.resolve(process.cwd(), "client/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(process.cwd(), "client/build/index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`Running on port ${PORT}`);
